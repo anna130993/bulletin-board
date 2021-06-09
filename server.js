b0fd2e05fef857839b36e7fabd1bda6818698df7
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-06-09 20:49:32 +0200

    Update the files

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
-const formidable = require('formidable');
 
 const postsRoutes = require('./routes/posts.routes');
 
@@ -10,7 +9,6 @@ const app = express();
 
 /* MIDDLEWARE */
 app.use(cors());
-app.use(formidable({uploadDir: 'public/images', keepExtensions: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -39,5 +37,5 @@ db.on('error', err => console.log('Error: ' + err));
 /* START SERVER */
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log('Server is running on port: '+port);
+  console.log('Server is running on port: ' + port);
 });
