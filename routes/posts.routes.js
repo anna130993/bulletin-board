3cc5467627f57cd72cca6097c57edf7f3933adba
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-06-10 16:49:56 +0200

    Add image filtering

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -14,7 +14,16 @@ const storage = multer.diskStorage({destination: (req, file, cb) => {
 },
 });
 
-const upload = multer ({storage: storage});
+const upload = multer ({storage: storage,
+  fileFilter: (req, file, cb) => {
+    if(!file.mimetype.includes('image')) {
+      return cb(new Error('Please make sure you are choosing an image file'));
+    } cb(null, true);
+  },
+  limits: {
+    fileSize: 99999999,
+  },
+});
 
 router.get('/posts', async (req, res) => {
   try {
@@ -62,14 +71,16 @@ router.post('/posts', upload.single('photo'), async (req, res) => {
 
 router.put('/posts/:id', upload.single('photo'), async (req, res) => {
   const {title, text, price, phone, location, status} = req.body;
+  let photoString = req.body.photo;
   const photo = req.file;
 
   if(titleVal(title) && textVal(text) && statVal(status) && photoVal(photo)) {
+    if (!photoString) photoString = photo ? photo.path.replace('public', '') : '';
     const date = new Date();
     try {
       const post = await Post.findById(req.params.id);
       if (post) {
-        Object.assign(post, {title, text, photo: photo ? photo.path.replace('public', '') : '', price: price === 'null' ? null : price, phone, status, location, updated: date});
+        Object.assign(post, {title, text, photo: photoString, price: price === 'null' ? null : price, phone, status, location, updated: date});
         const updatedPost = await post.save();
         res.json(updatedPost);
       }
